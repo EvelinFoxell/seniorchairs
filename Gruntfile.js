a9cf11809b4fe091251dc7494e2292da4174a61a
Erik Berkun-Drevnig <erik.b22@gmail.com>
2015-06-13 17:52:30 -0400

    Added under construction message paragraph tags Added favicon generation to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,13 +32,19 @@ module.exports = function(grunt) {
 					cache: false
 				}
 			}
+		},
+
+		exec: {
+			favicon: 'convert _favicon.svg -bordercolor white -border 0 \\( -clone 0 -resize 16x16 \\) \\( -clone 0 -resize 32x32 \\) \\( -clone 0 -resize 48x48 \\) \\( -clone 0 -resize 64x64 \\) -delete 0 -alpha off -colors 256 favicon.ico'
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-exec');
 
 	grunt.registerTask('default', ['concat', 'uglify']);
-	grunt.registerTask('images', ['imagemin:images'])
+	grunt.registerTask('images', ['imagemin:images']);
+	grunt.registerTask('favicon', ['exec:favicon']);
 };
