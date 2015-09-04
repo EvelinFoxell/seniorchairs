1c69435565ad388ba377cc98f4f5cb03969d898a
Erik Berkun-Drevnig <erik.b22@gmail.com>
2015-09-03 21:34:26 -0400

    Use URL to filter on load and change term tag to filter

--- assets/scripts/_source/filtering.js
+++ assets/scripts/_source/filtering.js
@@ -8,6 +8,13 @@ $(function() {
 	$('#items').mixItUp({
 		animation: {
 			enable: false
+		},
+		load: {
+			filter: window.location.hash ? '.' + (window.location.hash).replace('#', '') : 'all'
 		}
+	});
+
+	$('.filter').click(function() {
+		window.location.hash = $(this).attr('data-filter').replace('.', '#');
 	});
 });
